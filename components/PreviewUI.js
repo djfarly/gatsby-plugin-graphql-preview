7da6e2624e9c0663539818b4fb7d72f39e177dc0
Jan Willem Henckel <farly.de@gmail.com>
2019-02-23 21:54:44 +0100

    make preview ui draggable

--- components/PreviewUI.js
+++ components/PreviewUI.js
@@ -1,47 +1,58 @@
 const React = require('react');
 const PropTypes = require('prop-types');
+const Draggable = require('react-draggable');
+
+const style = {
+  container: {
+    position: 'fixed',
+    zIndex: Number.MAX_SAFE_INTEGER,
+    padding: '4px 6px',
+    borderRadius: 5,
+    border: '1px solid rgba(230, 230, 230, 0.5)',
+    backgroundColor: 'rgba(240, 240, 240, 0.75)',
+    boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.15)'
+  },
+  handle: {
+    height: 10,
+    width: 10,
+    backgroundColor: 'green'
+  }
+};
 
 function PreviewUI({ setPollInterval, pollInterval, refetch, loading, error }) {
   return (
-    <div
-      style={{
-        position: 'fixed',
-        zIndex: Number.MAX_SAFE_INTEGER,
-        top: 10,
-        right: 10,
-        padding: '4px 6px',
-        borderRadius: 5,
-        border: '1px solid rgba(230, 230, 230, 0.5)',
-        backgroundColor: 'rgba(240, 240, 240, 0.75)',
-        boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.15)'
-      }}
-    >
-      <div>
-        <strong>
-          Live preview {loading ? 'loading' : error ? 'errored' : 'active'}
-        </strong>
+    <Draggable handle=".handle" defaultPosition={{ x: 10, y: 10 }}>
+      <div style={style.container}>
+        <div className="handle" style={style.handle}>
+          …
+        </div>
+        <div>
+          <strong>
+            Live preview {loading ? 'loading' : error ? 'errored' : 'active'}
+          </strong>
+        </div>
+        <div>
+          <label>
+            Update every:{' '}
+            <select
+              onChange={e => {
+                setPollInterval(e.target.value);
+              }}
+              value={pollInterval}
+            >
+              <option value={200}>200ms</option>
+              <option value={1000}>1s</option>
+              <option value={10000}>10s</option>
+            </select>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="button" onClick={refetch} value="Update now" />
+          </label>
+        </div>
       </div>
-      <div>
-        <label>
-          Update every:{' '}
-          <select
-            onChange={e => {
-              setPollInterval(e.target.value);
-            }}
-            value={pollInterval}
-          >
-            <option value={200}>200ms</option>
-            <option value={1000}>1s</option>
-            <option value={10000}>10s</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="button" onClick={refetch} value="Update now" />
-        </label>
-      </div>
-    </div>
+    </Draggable>
   );
 }
 
